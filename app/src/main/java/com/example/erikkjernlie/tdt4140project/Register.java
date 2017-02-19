2d319360850558a133a68fc33a63d91fbf33f50c
Erik Kjernlie <erik_kjernlie@hotmail.com>
2017-02-19 17:27:41 +0100

    Registerclass update. Test to see if the pushing works.

--- app/src/main/java/com/example/erikkjernlie/tdt4140project/Register.java
+++ app/src/main/java/com/example/erikkjernlie/tdt4140project/Register.java
@@ -2,23 +2,54 @@ package com.example.erikkjernlie.tdt4140project;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
 
     private TextView textView;
+    private char gender;
+    private double grade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initGenderBtns();
 
 
 
 
+    }
 
 
 
-}
 
+    public void initGenderBtns(){
+        ImageButton femaleBtn = (ImageButton)findViewById(R.id.female);
+        ImageButton maleBtn = (ImageButton)findViewById(R.id.male);
+        femaleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gender = 'F';
+                Toast.makeText(Register.this, "Male", Toast.LENGTH_SHORT).show();
+                setContentView(R.layout.register_information);
+            }
+        });
+        maleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gender = 'M';
+                Toast.makeText(Register.this, "Female", Toast.LENGTH_SHORT).show();
+                setContentView(R.layout.register_information);
+            }
+        });
+
+    }
+
+    public char getGender() {
+        return gender;
+    }
 }
