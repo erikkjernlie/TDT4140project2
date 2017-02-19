7c038c1cda57b3a8399388adaf4e12f57b9bd3be
Erik Kjernlie <erik_kjernlie@hotmail.com>
2017-02-19 19:49:15 +0100

    Updating register-class. Enables you to choose gender, as well as typing in your already calculated score. Fixed a few small bugs.

--- app/src/main/java/com/example/erikkjernlie/tdt4140project/Register.java
+++ app/src/main/java/com/example/erikkjernlie/tdt4140project/Register.java
@@ -3,6 +3,8 @@ package com.example.erikkjernlie.tdt4140project;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,14 +21,9 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         initGenderBtns();
 
-
-
-
     }
-
-
-
-
+    //hvis vi vil ha muligheten til å gå ET HAKK tilbake, så kan vi ikke bare ha setContentView, da må vi vise til en ny Activity
+    //finner ut kjønn
     public void initGenderBtns(){
         ImageButton femaleBtn = (ImageButton)findViewById(R.id.female);
         ImageButton maleBtn = (ImageButton)findViewById(R.id.male);
@@ -34,20 +31,62 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 gender = 'F';
-                Toast.makeText(Register.this, "Male", Toast.LENGTH_SHORT).show();
-                setContentView(R.layout.register_information);
+                Toast.makeText(Register.this, "Female", Toast.LENGTH_SHORT).show();
+                setContentView(R.layout.q_grade);
+                findOutGrade(); //sender videre til karakterer
+
             }
         });
         maleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 gender = 'M';
-                Toast.makeText(Register.this, "Female", Toast.LENGTH_SHORT).show();
-                setContentView(R.layout.register_information);
+                Toast.makeText(Register.this, "Male", Toast.LENGTH_SHORT).show();
+                setContentView(R.layout.q_grade);
+                findOutGrade();
             }
         });
 
     }
+
+    public void findOutGrade(){
+        Button reg = (Button) findViewById(R.id.Registered);
+        Button notReg = (Button) findViewById(R.id.notRegistered);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.calculated_grades);
+                knowsGrade(); //hvis personen vet karakteren sendes man til denne metoden
+
+            }
+        });
+        notReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.register_grades);
+                //personen vet ikke karakteren sin
+            }
+        });
+
+    }
+
+    public void knowsGrade(){
+        Button b = (Button)findViewById(R.id.button3);
+        final EditText e = (EditText) findViewById(R.id.editText3);
+        final TextView t = (TextView)findViewById(R.id.textView3);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //her må vi ha med masse tester itilfelle input er string
+                grade = Double.valueOf(e.getText().toString());
+                t.setText(Double.toString(grade));
+
+            }
+        });
+
+
+    }
+
 
     public char getGender() {
         return gender;
--- app/src/main/java/com/example/erikkjernlie/tdt4140project/SplashScreen.java
+++ app/src/main/java/com/example/erikkjernlie/tdt4140project/SplashScreen.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 public class SplashScreen extends AppCompatActivity {
 
     //tid visning, splashscreen
-    private static int SPLASH_TIME_OUT = 2000;
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
