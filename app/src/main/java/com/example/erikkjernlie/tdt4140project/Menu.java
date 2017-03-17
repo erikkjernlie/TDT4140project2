70c356605b34bbc192eca57079d2f3905606567d
Erik Kjernlie <erik_kjernlie@hotmail.com>
2017-03-17 14:30:00 +0100

    Deleted the alert that appeared the first time you launched the app.

--- app/src/main/java/com/example/erikkjernlie/tdt4140project/Menu.java
+++ app/src/main/java/com/example/erikkjernlie/tdt4140project/Menu.java
@@ -1,12 +1,8 @@
 package com.example.erikkjernlie.tdt4140project;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,30 +12,7 @@ public class Menu extends AppCompatActivity {
     private Button explore; //explore button
     private Button aboutUnibot; //about button
 
-    public static final String PREFS_NAME = "MyPrefsFile";
 
-    //pop-up screen, appears only the first time the app is running
-    private void alert(){
-        AlertDialog.Builder builder1 = new AlertDialog.Builder(this, R.layout.overlay);
-        LayoutInflater inflater = this.getLayoutInflater();
-
-        // Inflate and set the layout for the dialog
-        // Pass null as the parent view because its going in the dialog layout
-        builder1.setView(inflater.inflate(R.layout.overlay, null));
-        builder1.setTitle("Welcome to uniBOT");
-
-
-        builder1.setPositiveButton("Confirm",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
-
-
-        AlertDialog alert11 = builder1.create();
-        alert11.show();
-    }
 
     public void initButtons(){
         register = (Button) findViewById(R.id.register);
@@ -74,18 +47,6 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         initButtons();
-        //uncomment this when we want the alert just to appear the first time the app is started
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
 
-        boolean dialogShown = settings.getBoolean("dialogShown", false);
-
-        if (!dialogShown) {
-            // AlertDialog code here
-        alert();
-
-            SharedPreferences.Editor editor = settings.edit();
-            editor.putBoolean("dialogShown", true);
-            editor.commit();
-        }
     }
 }
