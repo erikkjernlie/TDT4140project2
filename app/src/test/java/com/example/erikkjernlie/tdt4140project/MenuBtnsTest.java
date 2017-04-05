8b9bce6e08924592e84e10db91d0ac9a7933cf2e
Jorgen Mortensen <jorgen.bm96@gmail.com>
2017-04-05 16:32:34 +0200

    Created menu_test and sign_in_test so that we can now test the buttons in the activities

---
+++ app/src/test/java/com/example/erikkjernlie/tdt4140project/MenuBtnsTest.java
@@ -0,0 +1,8 @@
+package com.example.erikkjernlie.tdt4140project;
+
+/**
+ * Created by Jørgen on 04.04.2017.
+ */
+
+public class MenuBtnsTest {
+}
---
+++ app/src/test/java/com/example/erikkjernlie/tdt4140project/Sign_inBtnTest.java
@@ -0,0 +1,8 @@
+package com.example.erikkjernlie.tdt4140project;
+
+/**
+ * Created by Jørgen on 05.04.2017.
+ */
+
+public class Sign_inBtnTest {
+}
