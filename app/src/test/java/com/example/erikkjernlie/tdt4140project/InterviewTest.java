83972e7c53cc1ff1079be73e27743d19d00de82d
Jorgen Mortensen <jorgen.bm96@gmail.com>
2017-04-19 09:37:33 +0200

    Created Interview-test and updated UserInfoTest

---
+++ app/src/test/java/com/example/erikkjernlie/tdt4140project/InterviewTest.java
@@ -0,0 +1,27 @@
+package com.example.erikkjernlie.tdt4140project;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created by Jørgen on 19.04.2017.
+ */
+
+public class InterviewTest {
+
+    Interview intView;
+
+    @Test(expected = IllegalArgumentException.class)
+    public void stateFailTest() throws Exception {
+        assertNotNull(intView = new Interview());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        intView = null;
+    }
+
+}
